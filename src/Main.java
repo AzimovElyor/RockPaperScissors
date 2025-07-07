450096d30915add58634b7d01810d7f433e3ab0b
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-07-07 21:25:09 +0500

    Hello1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world2");
+        System.out.println("Hello world1");
 
         // Rock , paper, scissors  ekrandan olib olamiz  +++
 
