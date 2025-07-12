162634a70201bdf6e25664293be6c26c42fb1c8b
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-07-12 17:54:25 +0500

    Project is updated by adding cycle operators(do while);

--- src/Main.java
+++ src/Main.java
@@ -4,43 +4,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world1");
-
-        // Rock , paper, scissors  ekrandan olib olamiz  +++
-
-        // computer tanlagan tanlovini  ===> 0 ==> rock,  1 ==> paper, 2 ==> scissors +++
-
-        // tanlagan tanlomizni aniqlashtirib oldik  +++
-
-        // biz tanlagan tanlovlardan computerniki solishtirish
-
-        // result ==> You win! You lose! Draw
-
-        // rock >  scissors , rock < paper , rock == rock +++
-
-        // paper > rock, paper < scissors, paper == paper +++
-
-        // scissors > paper, scissors < rock, scissors == scissors +++
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         Random random = new Random(); // random qiymalar generatsiya qilib berish uchun
-        System.out.print(" Enter only rock, paper, or scissors ==> ");
-        String myChoice = scanner.nextLine();
+        int winNumber = 0;
+        int lossNumber = 0;
+        int drawNumber = 0;
+        String myChoice = "";
         String result = "";
+do{
+    System.out.print(" Enter only rock, paper, or scissors, tugash ==> ");
+    myChoice = scanner1.next();
+    String computerChoice;
+    int randomNumber = random.nextInt(0, 3);
 
-        String computerChoice = "";
-        int randomNumber = random.nextInt(0, 3);
-
-        computerChoice = switch (randomNumber){
-            case 0 -> "rock";  // rock
-            case 1 -> "paper"; // paper
-            case 2 -> "scissors";
-            default-> "Invalid generated value";
-        };
+    computerChoice = switch (randomNumber){
+        case 0 -> "rock";  // rock
+        case 1 -> "paper"; // paper
+        case 2 -> "scissors";
+        default-> "Invalid generated value";
+    };
 
 
 /*        System.out.println("myChoice = " + myChoice);
         System.out.println("computerChoice = " + computerChoice);*/
-        System.out.println("randomNumber = " + randomNumber);
+    System.out.println("randomNumber = " + randomNumber);
    /*     switch(randomNumber){
             case 0: computerChoice = "rock"; break;
             case 1: computerChoice = "paper"; break;
@@ -48,7 +35,7 @@ public class Main {
             default:
                 System.out.println("Invalid generated number");
         }*/
-        // if else
+    // if else
 
 /*        if(myChoice.equals("rock")){
 
@@ -57,50 +44,64 @@ public class Main {
         }else{
 
         }*/
-   // switch
-        if(myChoice.equals("paper") || myChoice.equals("rock") ||myChoice.equals("scissors")) {
-            switch (myChoice) {
-                case "rock": {  // rock > computerchoice = scissors, rock < computerChoise = paper , rock = rock
-                    if (computerChoice.equals("scissors")) {
-                        result = "You win!";
-                    } else if (computerChoice.equals("paper")) {
-                        result = "You lost!";
-                    } else {
-                        result = "Draw";
-                    }
+    // switch
+    if(myChoice.equals("paper") || myChoice.equals("rock") ||myChoice.equals("scissors")) {
+        switch (myChoice) {
+            case "rock": {  // rock > computerchoice = scissors, rock < computerChoise = paper , rock = rock
+                if (computerChoice.equals("scissors")) {
+                    result = "You win!";
+                    winNumber++;
+                } else if (computerChoice.equals("paper")) {
+                    result = "You lost!";
+                    lossNumber++;
+                } else {
+                    result = "Draw";
+                    drawNumber++;
                 }
-                break;
-                case "paper": {
-                    if (computerChoice.equals("rock")) {
-                        result = "You win!";
-                    } else if (computerChoice.equals("scissors")) {
-                        result = "You lost!";
-                    } else {
-                        result = "Draw";
-                    }
-
+            }
+            break;
+            case "paper": {
+                if (computerChoice.equals("rock")) {
+                    result = "You win!";
+                    winNumber++;
+                } else if (computerChoice.equals("scissors")) {
+                    result = "You lost!";
+                    lossNumber++;
+                } else {
+                    result = "Draw";
+                    drawNumber++;
                 }
-                break;
-                case "scissors": {
-                    if (computerChoice.equals("paper")) {
-                        result = "You win!";
-                    } else if (computerChoice.equals("rock")) {
-                        result = "You lost!";
-                    } else {
-                        result = "Draw";
-                    }
-                }
-                break;
-                default:
-                    System.out.println("Invalid choice");
 
             }
+            break;
+            case "scissors": {
+                if (computerChoice.equals("paper")) {
+                    result = "You win!";
+                    winNumber++;
+                } else if (computerChoice.equals("rock")) {
+                    result = "You lost!";
+                    lossNumber++;
+                } else {
+                    result = "Draw";
+                    drawNumber++;
+                }
+            }
+            break;
+            default:
+                System.out.println("Invalid choice");
 
-            System.out.println("Your choie is " + myChoice + " and the computer choice is " + computerChoice + " and the result is " + result);
-        }else{
-            System.out.println("Invalid my choice");
         }
 
+        System.out.println("Your choie is " + myChoice + " and the computer choice is " + computerChoice + " and the result is " + result);
+    }else{
+        System.out.println("Invalid my choice");
+    }
+    System.out.println("Win Number = " + winNumber + " and Loss Number = " + lossNumber + " and Draw Number = " + drawNumber);
+
+
+}while(!myChoice.equals("tugash")); // tugash == tugash
+
+        System.out.println("The game ends");
 
 
 
