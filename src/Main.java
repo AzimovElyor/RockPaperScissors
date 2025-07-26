858b18e921582d3f7a3502936cc82bc7ad0fc6d7
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-07-26 17:52:28 +0500

    Project is updated by adding methods

--- src/Main.java
+++ src/Main.java
@@ -2,111 +2,101 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+   public static int winNumber = 0;
+    public static int loseNumber = 0;
+    public static int drawNumber = 0;
+    public static  final String WIN_MESSAGE = "You win!";
+    public static final String LOST_MESSAGE  = "You lost!";
+    public static final String DRAW_MESSAGE = "DRAW";
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String END = "tugash";
     public static void main(String[] args) {
-        System.out.println("Hello world1");
         Scanner scanner1 = new Scanner(System.in);
         Random random = new Random(); // random qiymalar generatsiya qilib berish uchun
-        int winNumber = 0;
-        int lossNumber = 0;
-        int drawNumber = 0;
         String myChoice = "";
         String result = "";
 do{
-    System.out.print(" Enter only rock, paper, or scissors, tugash ==> ");
-    myChoice = scanner1.next();
-    if(myChoice.equals("tugash")){
-        break;
-    }
-    String computerChoice;
-    int randomNumber = random.nextInt(0, 3);
+    myChoice = getMyChoice(scanner1);  // get my choice
+    if(myChoice.equals(END)) break;
 
-    computerChoice = switch (randomNumber){
-        case 0 -> "rock";  // rock
-        case 1 -> "paper"; // paper
-        case 2 -> "scissors";
-        default-> "Invalid generated value";
-    };
+    String computerChoice = getComputerChoice(random);  //  computerchoice
 
-
-/*        System.out.println("myChoice = " + myChoice);
-        System.out.println("computerChoice = " + computerChoice);*/
-    System.out.println("randomNumber = " + randomNumber);
-   /*     switch(randomNumber){
-            case 0: computerChoice = "rock"; break;
-            case 1: computerChoice = "paper"; break;
-            case 2: computerChoice = "scissors"; break;
-            default:
-                System.out.println("Invalid generated number");
-        }*/
-    // if else
-
-/*        if(myChoice.equals("rock")){
-
-        }else if(myChoice.equals("paper")){
-
-        }else{
-
-        }*/
-    // switch
-    if(myChoice.equals("paper") || myChoice.equals("rock") ||myChoice.equals("scissors")) {
-        switch (myChoice) {
-            case "rock": {  // rock > computerchoice = scissors, rock < computerChoise = paper , rock = rock
-                if (computerChoice.equals("scissors")) {
-                    result = "You win!";
-                    winNumber++;
-                } else if (computerChoice.equals("paper")) {
-                    result = "You lost!";
-                    lossNumber++;
-                } else {
-                    result = "Draw";
-                    drawNumber++;
-                }
-            }
-            break;
-            case "paper": {
-                if (computerChoice.equals("rock")) {
-                    result = "You win!";
-                    winNumber++;
-                } else if (computerChoice.equals("scissors")) {
-                    result = "You lost!";
-                    lossNumber++;
-                } else {
-                    result = "Draw";
-                    drawNumber++;
-                }
-
-            }
-            break;
-            case "scissors": {
-                if (computerChoice.equals("paper")) {
-                    result = "You win!";
-                    winNumber++;
-                } else if (computerChoice.equals("rock")) {
-                    result = "You lost!";
-                    lossNumber++;
-                } else {
-                    result = "Draw";
-                    drawNumber++;
-                }
-            }
-            break;
-            default:
-                System.out.println("Invalid choice");
-
-        }
+    if(myChoice.equals(PAPER) || myChoice.equals(ROCK) || myChoice.equals(SCISSORS)) {
+        result = switch (myChoice) {
+            case ROCK -> casesForRock(computerChoice);
+            case PAPER -> casesForPaper(computerChoice);
+            case SCISSORS -> casesForScissors(computerChoice);
+            default -> result;
+        };
 
         System.out.println("Your choie is " + myChoice + " and the computer choice is " + computerChoice + " and the result is " + result);
     }else{
         System.out.println("Invalid my choice");
+        printResult();
     }
-    System.out.println("Win Number = " + winNumber + " and Loss Number = " + lossNumber + " and Draw Number = " + drawNumber);
 
-
-}while(true); // tugash == tugash
+}while(true);
 
         System.out.println("The game ends ");
 
 
 
     }
+    public static String getMyChoice(Scanner scanner){
+        System.out.print(" Enter only rock, paper, or scissors, tugash ==> ");
+        return scanner.next();
+    }
+    public static String getComputerChoice(Random random){
+        int randomNumber = random.nextInt(0, 3);
+
+        return switch (randomNumber){
+            case 0 -> ROCK;  // rock
+            case 1 -> PAPER; // paper
+            case 2 -> SCISSORS;
+            default-> "Invalid generated value";
+        };
+    }
+    public static String casesForRock(String computerChoice){
+        if (computerChoice.equals(SCISSORS)) {
+            winNumber++;
+            return   WIN_MESSAGE;
+        } else if (computerChoice.equals(PAPER)) {
+            loseNumber++;
+            return LOST_MESSAGE;
+        } else {
+            drawNumber++;
+            return  DRAW_MESSAGE;
+        }
+    }
+    public static String casesForPaper(String computerChoice){
+        if (computerChoice.equals(ROCK)) {
+            winNumber++;
+            return  WIN_MESSAGE;
+
+        } else if (computerChoice.equals(SCISSORS)) {
+            loseNumber++;
+            return  LOST_MESSAGE;
+        } else {
+            drawNumber++;
+             return  DRAW_MESSAGE;
+        }
+    }
+    public static String casesForScissors(String computerChoice){
+        if (computerChoice.equals(PAPER)) {
+            winNumber++;
+            return WIN_MESSAGE;
+        } else if (computerChoice.equals(ROCK)) {
+            loseNumber++;
+            return LOST_MESSAGE;
+        } else {
+            drawNumber++;
+            return  DRAW_MESSAGE;
+        }
+    }
+    public static void printResult(){
+        System.out.println("Win Number = " + winNumber + " and Loss Number = " + loseNumber + " and Draw Number = " + drawNumber);
+    }
+
 }
